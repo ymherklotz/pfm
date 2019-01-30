7ea4189e8643e4083ff345b0949e33d54371fe98
Yann Herklotz <ymherklotz@gmail.com>
2019-01-30 20:28:25 +0000

    Clean up Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,7 @@
 module Main where
 
-import           Criterion
-import qualified Data.ByteString      as B
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Text            as T
-import           Data.Word            (Word8)
 import           PFM
 
-clamp :: PFMColour -> PPMColour
-clamp (PFMColour ri gi bi) =
-  PPMColour (f ri) (f gi) (f bi)
-  where
-    v s = s * 255.0
-    f s = if v s > 255.0 then 255 else fromInteger (round (v s))
-clamp _ = undefined
-
-clampImage :: PFMImage -> PPMImage
-clampImage (PFMImage w h c) =
-  PPMImage w h . reverse $ fmap clamp <$> c
-
 main :: IO ()
-main = do
-  -- s <- B.readFile "/home/yannherklotz/Imperial/AdvancedGraphics/coursework1/CO417-Assignment1/UrbanProbe/urbanEM_latlong.pfm"
-  s <- B.readFile "/home/yannherklotz/Downloads/memorial.pfm"
-  BL.writeFile "random.ppm" . encodePPM . clampImage . parse $ s
+main =
+  putStrLn "Work in progress..."
